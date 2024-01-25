00f63a9b4b423fbde9ebcda68f4e77b3a684f608
samiullah <sami606713@gmail.com>
2024-01-25 22:33:01 +0500

    Now predict the price

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from utils import read_data,load_image
-
+from utils import read_data,load_image,load_model
+import numpy as np
+import pandas as pd
 # df=read_data("src/Notebook/data.csv")
 # st.dataframe(df)
 def main():
@@ -14,42 +15,73 @@ def main():
 
         # left col
         with left_col:
-            st.title("Image")
-            img=load_image("src/Notebook/2pie.jpeg")
+            # st.title("Image")
+            img=load_image("src/Notebook/house.jpg")
             st.image(img)
 
         
         # Right col
         with right_col:
             # Make a form
-            with st.form("House Form"):
-                st.title("load")
+            with st.form("House Predict Form"):
+                # st.title("load")
                 
                 # Add a text input field
                 area=st.number_input("Enter Area", format="%d",min_value=0)
                 
+                # Load the dataset
                 df=read_data("src/Notebook/clean_data.csv")
-                """
-                add rauio btn heres
                 
-                """
-                
-                bed,bath,flor=st.columns(3)
+                bed,bath,floor=st.columns(3)
                 with bed:
                     # Bedrooms
-                    st.selectbox("Bedrooms", sorted(df[df["bedrooms"]>0]["bedrooms"].unique()))
+                    bedroom=st.selectbox("Bedrooms", sorted(df[df["bedrooms"]>0]["bedrooms"].unique()))
                
                 # Bath
                 with bath:
-                    st.selectbox("bath", sorted(df[df["bathrooms"]>0]["bathrooms"].unique()))
+                    bathroom=st.selectbox("bath", sorted(df[df["bathrooms"]>0]["bathrooms"].unique()))
                 
-                with flor:
-                    st.selectbox("floor", sorted(df[df["floors"]>0]["floors"].unique()))
+                with floor:
+                    floors=st.selectbox("floor", sorted(df[df["floors"]>0]["floors"].unique()))
+            
+
+                view,water=st.columns(2)
+                with view:
+                    views=st.selectbox("view", sorted(df[df["view"]>0]["view"].unique()))
+
+                with water:
+                    water_front=st.selectbox("waterfront", sorted(df["waterfront"].unique()))
+
                 # Use st.selectbox to create a dropdown
-                selected_city = st.selectbox("location", df["city"])
+                count,city=st.columns(2)
+                with count:
+                    country=st.selectbox("location", df["country"].unique())
+                with city:
+                    selected_city = st.selectbox("location", df["city"])
 
                 # form submit button
-                st.form_submit_button("predict")
+                if(st.form_submit_button("Estimate Price")):
+                    
+                    # get the values and make a dataframe b/c columns transformer espect dataframe not a numpy array
+                    data=np.array([[bedroom,bathroom,floors,water_front,views,selected_city,country,area]])
+                    df = pd.DataFrame({
+                    'bedrooms': [bedroom],
+                    'bathrooms': [bathroom],
+                    'floors': [floors],
+                    'waterfront': [water_front],
+                    'view': [views],
+                    'city': [selected_city],
+                    'country': [country],
+                    'sqft_total': [area]
+                    })
+                    # load the model
+                    model=load_model("src/Model/house.pkl")
+                    
+                    
+                    st.dataframe(df)
+                    pred=model.predict(df)
+                    st.success(f"House preice is {pred[0].round()}")
+                    
 
 if __name__=="__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 from PIL import Image
-
+import requests as r
+from io import BytesIO
+from joblib import load
 # Read dataset
 def read_data(file_path):
     df=pd.read_csv(file_path)
@@ -9,5 +11,14 @@ def read_data(file_path):
 
 # Load the image
 def load_image(img_path):
+
     img=Image.open(img_path)
+
     return img
+
+def load_model(path):
+    model=load(path)
+    return model
+
+# if __name__=="__main__":
+#     model=load_model("src/Model/house.pkl")
